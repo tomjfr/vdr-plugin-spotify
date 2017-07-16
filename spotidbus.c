561f8b89243b1268504099663af7a603b5e47801
Johann Friedrichs <johann.friedrichs@web.de>
2017-07-16 12:43:08 +0200

    Add GPL

--- spotidbus.c
+++ spotidbus.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2016-2017 Johann Friedrichs
+ *
+ * This file is part of vdr-plugin-spotify.
+ *
+ * vdr-plugin-spotify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * vdr-plugin-spotify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with vdr-plugin-spotify.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+
 #include <string.h>
 #include "spotidbus.h"
 
--- spotify.c
+++ spotify.c
@@ -1,11 +1,21 @@
-
 /*
- * spotify.c: A plugin for the Video Disk Recorder
+ * Copyright (C) 2016-2017 Johann Friedrichs
  *
- * See the README file for copyright information and how to reach the author.
+ * This file is part of vdr-plugin-spotify.
  *
- * $Id$
- */
+ * vdr-plugin-spotify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * vdr-plugin-spotify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with vdr-plugin-spotify.  If not, see <http://www.gnu.org/licenses/>.
+ * */
 
 #include <vdr/plugin.h>
 #include <vdr/osd.h>
